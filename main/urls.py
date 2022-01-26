1178d296ce195ae6aef7367dbdac450839ac7d70
Michael <michaelodell660@gmail.com>
2022-01-26 14:10:29 -0800

    added POST and PUT routes for recipes

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('recipes/', views.getRecipes, name="recipes"),
     path('recipes/<str:pk>', views.getRecipes, name="recipes"),
+    path('recipes/create/', views.makeRecipe, name="new_recipe"),
+    path('recipes/update/<str:pk>/', views.updateRecipe, name="upate_recipe")
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -35,3 +36,21 @@ def getRecipes(request, pk=None):
 
     return Response(serializer.data) 
 
+@api_view(['POST'])
+def makeRecipe(request):
+    serializer = RecipeSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def updateRecipe(request, pk):
+    recipe = Recipe.objects.get(id=pk)
+    serializer = RecipeSerializer(instance=recipe, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response({
+        'data': serializer.data
+        }, status=status.HTTP_202_ACCEPTED)
